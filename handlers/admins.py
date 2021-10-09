0204e2d5c306df5f0721e64056353b07bbb98da4
ALLHB <74823117+ALLHB@users.noreply.github.com>
2021-10-09 22:32:45 +0300

    Add files via upload

---
+++ handlers/admins.py
@@ -0,0 +1,128 @@
+# Copyright (C) 2021 xV00V
+
+
+from asyncio import QueueEmpty
+from config import que
+from pyrogram import Client, filters
+from pyrogram.types import Message
+
+from cache.admins import admins
+from helpers.channelmusic import get_chat_id
+from helpers.decorators import authorized_users_only, errors
+from helpers.filters import command, other_filters
+from callsmusic import callsmusic
+from callsmusic.queues import queues
+
+
+@Client.on_message(filters.command("reload"))
+async def update_admin(client, message):
+    global admins
+    new_admins = []
+    new_ads = await client.get_chat_members(message.chat.id, filter="administrators")
+    for u in new_ads:
+        new_admins.append(u.user.id)
+    admins[message.chat.id] = new_admins
+    await message.reply_text("✔ bot ** تم إعادة تحميله بشكل صحيح! ** \ n✔ ** تم تحديث قائمة المشرف ** **! **")
+
+
+@Client.on_message(command("pause") & other_filters)
+@errors
+@authorized_users_only
+async def pause(_, message: Message):
+    chat_id = get_chat_id(message.chat)
+    if (chat_id not in callsmusic.pytgcalls.active_calls) or (
+        callsmusic.pytgcalls.active_calls[chat_id] == "paused"
+    ):
+        await message.reply_text("✘ قائمة التشغيل فارغة !")
+    else:
+        callsmusic.pytgcalls.pause_stream(chat_id)
+        await message.reply_text("✔ توقفت الموسيقى مؤقتًا !")
+
+
+@Client.on_message(command("resume") & other_filters)
+@errors
+@authorized_users_only
+async def resume(_, message: Message):
+    chat_id = get_chat_id(message.chat)
+    if (chat_id not in callsmusic.pytgcalls.active_calls) or (
+        callsmusic.pytgcalls.active_calls[chat_id] == "playing"
+    ):
+        await message.reply_text("✘ لم يتم إيقاف أي شيء مؤقتًا!")
+    else:
+        callsmusic.pytgcalls.resume_stream(chat_id)
+        await message.reply_text("✔ استؤنفت الموسيقى!")
+
+
+@Client.on_message(command("end") & other_filters)
+@errors
+@authorized_users_only
+async def stop(_, message: Message):
+    chat_id = get_chat_id(message.chat)
+    if chat_id not in callsmusic.pytgcalls.active_calls:
+        await message.reply_text("✘ قائمة التشغيل فارغة !")
+    else:
+        try:
+            queues.clear(chat_id)
+        except QueueEmpty:
+            pass
+
+        callsmusic.pytgcalls.leave_group_call(chat_id)
+        await message.reply_text("✔ تم الايقاف !")
+
+
+@Client.on_message(command("skip") & other_filters)
+@errors
+@authorized_users_only
+async def skip(_, message: Message):
+    global que
+    chat_id = get_chat_id(message.chat)
+    if chat_id not in callsmusic.pytgcalls.active_calls:
+        await message.reply_text("✘ قائمة التشغيل فارغة !")
+    else:
+        queues.task_done(chat_id)
+
+        if queues.is_empty(chat_id):
+            callsmusic.pytgcalls.leave_group_call(chat_id)
+        else:
+            callsmusic.pytgcalls.change_stream(
+                chat_id, queues.get(chat_id)["file"]
+            )
+
+    qeue = que.get(chat_id)
+    if qeue:
+        skip = qeue.pop(0)
+    if not qeue:
+        return
+    await message.reply_text(f"✘ تم تخطي : **{skip[0]}**\n✔ تشغيل الان : **{qeue[0][0]}**")
+
+
+@Client.on_message(filters.command("auth"))
+@authorized_users_only
+async def authenticate(client, message):
+    global admins
+    if not message.reply_to_message:
+        await message.reply("✘ الرد على الرسالة لرفع العضو !")
+        return
+    if message.reply_to_message.from_user.id not in admins[message.chat.id]:
+        new_admins = admins[message.chat.id]
+        new_admins.append(message.reply_to_message.from_user.id)
+        admins[message.chat.id] = new_admins
+        await message.reply("user authorized.")
+    else:
+        await message.reply("✔ العضو لدية صلاحية مسبقآ !")
+
+
+@Client.on_message(filters.command("deauth"))
+@authorized_users_only
+async def deautenticate(client, message):
+    global admins
+    if not message.reply_to_message:
+        await message.reply("✘ الرد على الرسالة لتنزيل العضو !")
+        return
+    if message.reply_to_message.from_user.id in admins[message.chat.id]:
+        new_admins = admins[message.chat.id]
+        new_admins.remove(message.reply_to_message.from_user.id)
+        admins[message.chat.id] = new_admins
+        await message.reply("user deauthorized")
+    else:
+        await message.reply("✔ العضو ليس لدية اي صلاحية!")
